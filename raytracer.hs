f42fd9ac2e5776a6b6a60e42c2f1858084724a75
nicholashaydensmith <thesmithdynasty@gmail.com>
2014-08-12 17:39:58 -0700

    Yay we have a working product, now for the raytracing

--- raytracer.hs
+++ raytracer.hs
@@ -12,51 +12,30 @@ t3 [a, b, c] = (a, b, c)
 type OBJVertex = (GLfloat, GLfloat, GLfloat)
 type OBJFace = (OBJVertex, OBJVertex, OBJVertex)
 type Id = String
-type FrameBuffer = PixelData (Color3 GLubyte)
+type FrameBuffer = PixelData (Color4 GLubyte)
 data Obj = Geometry Id [OBJVertex] [OBJFace]
       deriving (Show)
 
+--main :: IO ()
+--main = do
+--      (_progName, _args) <- getArgsAndInitialize
+--      _window <- createWindow "Rayskell"
+--      clearColor $= Color4 0 0 0 0
+--      rowAlignment Unpack $= 1
+--      fb <- render (replicate (64 * 64) (Color4 0 0 255 0))
+--      displayCallback $= display fb
+--      mainLoop
 main :: IO ()
-main = do
-      (_progName, _args) <- getArgsAndInitialize
-      _window <- createWindow "Rayskell"
-      img <- myInit
-      displayCallback $= display img
-      mainLoop
+main = getArgsAndInitialize >>= \_ -> createWindow "Rayskell" >>= \_ -> render (replicate (64 * 64) (Color4 255 0 0 255)) >>= \fb -> displayCallback $= display fb >>= \_ -> mainLoop
 
 display :: FrameBuffer -> DisplayCallback
 display fb = do
       clear [ ColorBuffer ]
-      drawPixels checkImageSize fb
+      drawPixels (Size 64 64) fb
       flush
 
-checkImageSize :: Size
-checkImageSize = Size 64 64
-
-makeCheckImage :: Size -> GLsizei -> (GLubyte -> (Color3 GLubyte)) -> IO FrameBuffer
-makeCheckImage (Size w h) n f =
-                              fmap (PixelData RGB UnsignedByte) $
-                                    newArray [ f c |
-                                          i <- [ 0 .. w - 1 ],
-                                          j <- [ 0 .. h - 1 ],
-                                          let c | (i .&. n) == (j .&. n) = 0
-                                                | otherwise              = 255 ]
-
-myInit :: IO FrameBuffer
-myInit = do
-      clearColor $= Color4 0 0 0 0
-      rowAlignment Unpack $= 1
-      makeCheckImage checkImageSize 0x8 (\c -> Color3 c c c)
-
-render :: FrameBuffer -> [Obj] -> IO ()
-render fb []                     = putStr $ show fb
-render fb ((Geometry _ _ fs):gs) = render fb gs
-
---renderGeometry :: ImgData -> [OBJFace] -> ImgData
---renderGeometry v fs = map (\(PixelRGBA8 r g b a) -> PixelRGBA8 255 g b 255) v
-
---renderPixel :: PixelBaseComponent PixelRGBA8 -> PixelBaseComponent PixelRGBA8
---renderPixel a = a + 10
+render :: [Color4 GLubyte] -> IO FrameBuffer
+render fb = fmap (PixelData RGBA UnsignedByte) (newArray fb)
 
 parseObj :: Obj -> [String] -> Obj
 parseObj g []                         = g
